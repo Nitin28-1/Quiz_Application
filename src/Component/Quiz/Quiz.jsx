48407f2314006382753b47e31115bc27202de886
[nitinnegi3112h@gmail.com] <[nitinnegi3112h@gmail.com]>
2024-07-06 18:45:31 +0530

    upload file

--- src/Component/Quiz/Quiz.jsx
+++ src/Component/Quiz/Quiz.jsx
@@ -95,7 +95,7 @@ const Quiz = () => {
 
     <div className='w-[90%] relative my-3 mx-auto md:mx-[10%] '>
 
-     <div className='w-[100%] md:w-[90%] '>
+     <div className='w-[100%] flex flex-col gap-5 md:w-[90%] '>
     { 
         question.map((question,index) => (
             <div key={question.id} className='w-[100%] py-4 px-4 shadow-sm border border-gray-200 rounded'>
@@ -128,7 +128,7 @@ const Quiz = () => {
         ))
     }
 
-    <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-4 " onClick={()=>{handleSubmit(setloading,timerIntervalId,answer,setScore,setStatus,setShowResult,question)}}>Submit Quiz</button>
+    <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-4 w-[200px] " onClick={()=>{handleSubmit(setloading,timerIntervalId,answer,setScore,setStatus,setShowResult,question)}}>Submit Quiz</button>
     
    </div>
 
